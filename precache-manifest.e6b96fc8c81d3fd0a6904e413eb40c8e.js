d790def5d40fc690d4a109b29434dd31724c8883
luiz-j <jose.iscosistemas@gmail.com>
2020-04-28 15:44:04 -0700

    Updates

---
+++ precache-manifest.e6b96fc8c81d3fd0a6904e413eb40c8e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4688d3c24dd7d23b85097565c17e68",
+    "url": "/game-rock-papper-scissor/index.html"
+  },
+  {
+    "revision": "ffcd9e9193ec8ab37ba9",
+    "url": "/game-rock-papper-scissor/static/css/main.5b4aa913.chunk.css"
+  },
+  {
+    "revision": "b0c28b2cb7d1ff4ae21c",
+    "url": "/game-rock-papper-scissor/static/js/2.1e8d6081.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-rock-papper-scissor/static/js/2.1e8d6081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcd9e9193ec8ab37ba9",
+    "url": "/game-rock-papper-scissor/static/js/main.527afa83.chunk.js"
+  },
+  {
+    "revision": "ef83253a9fe20dcd0fa9",
+    "url": "/game-rock-papper-scissor/static/js/runtime-main.08ffa386.js"
+  },
+  {
+    "revision": "d0402957b1e2c95f299ac7d38e4e94be",
+    "url": "/game-rock-papper-scissor/static/media/back.d0402957.jpg"
+  },
+  {
+    "revision": "cbb9303f2fa7bb16505c8e844967e617",
+    "url": "/game-rock-papper-scissor/static/media/papper.cbb9303f.png"
+  },
+  {
+    "revision": "4ab7ed7509f8563b5e2ddc4f4d840955",
+    "url": "/game-rock-papper-scissor/static/media/scissors.4ab7ed75.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/game-rock-papper-scissor/precache-manifest.e6b96fc8c81d3fd0a6904e413eb40c8e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-rock-papper-scissor/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
